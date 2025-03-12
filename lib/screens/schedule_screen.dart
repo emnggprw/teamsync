abbe7ce27991f2ee664b550231ad4777d020b8ec
Eko <eko.mnggprw@gmail.com>
2025-03-12 19:11:49 +0700

    Added ability to add events on schedule

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class ScheduleScreen extends StatelessWidget {
+class ScheduleScreen extends StatefulWidget {
+  @override
+  _ScheduleScreenState createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<DateTime, List<String>> _events = {};
+
+  List<String> _getEventsForDay(DateTime day) {
+    return _events[day] ?? [];
+  }
+
+  void _addEvent(DateTime day, String event) {
+    if (_events[day] != null) {
+      _events[day]!.add(event);
+    } else {
+      _events[day] = [event];
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -9,11 +31,33 @@ class ScheduleScreen extends StatelessWidget {
       children: [
         Text('Schedule', style: Theme.of(context).textTheme.titleLarge),
         SizedBox(height: 10),
+        TableCalendar(
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 12, 31),
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+          eventLoader: _getEventsForDay,
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+            if (_selectedDay != null) {
+              _addEvent(_selectedDay!, 'New Event at ${_selectedDay!.toLocal()}');
+            }
+          },
+          child: Text('Add Event'),
+        ),
         Expanded(
-          child: TableCalendar(
-            firstDay: DateTime.utc(2020, 1, 1),
-            lastDay: DateTime.utc(2030, 12, 31),
-            focusedDay: DateTime.now(),
+          child: ListView(
+            children: _getEventsForDay(_selectedDay ?? _focusedDay)
+                .map((event) => ListTile(title: Text(event)))
+                .toList(),
           ),
         ),
       ],
